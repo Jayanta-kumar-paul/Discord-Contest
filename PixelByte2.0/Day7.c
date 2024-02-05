2929be2f66a4aeb565033bbda9dd40004f8cb080
Jayanta-kumar-paul <kumarpaul912@gmail.com>
2024-02-05 22:52:10 +0530

    new file

---
+++ PixelByte2.0/Day7.c
@@ -0,0 +1,30 @@
+#include <stdio.h> 
+int main() 
+{ 
+    int rows = 5; 
+    for (int i = 0; i < rows; i++)
+     { 
+        for (int j = 0; j < (rows - i) ; j++)
+        { 
+            printf(" "); 
+        } 
+        for (int k = 0; k <=  i ; k++) 
+        { 
+            printf("* "); 
+        } 
+        printf("\n"); 
+    } 
+    for (int i = rows; i >= 0; i--)
+    { 
+        for (int j =(rows-i); j >0 ; j--) 
+        { 
+            printf(" "); 
+        } 
+        for (int k = i; k >=0 ; k--) 
+        { 
+            printf("* "); 
+        } 
+            printf("\n");
+    }
+    return 0; 
+}
