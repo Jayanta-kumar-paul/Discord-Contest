b58ca5abaaee00b971286b3a23d3f94aaf89039a
Jayanta-kumar-paul <kumarpaul912@gmail.com>
2024-03-23 23:01:52 +0530

    new

--- PixalByte2.A/Day4.c
+++ PixalByte2.A/Day4.c
@@ -1,86 +1,21 @@
-// // C Program to print the hourglass pattern using numbers
-// #include <stdio.h>
-
-// int main()
-// {
-
-//     int n = 5;
-//     // first outer loop to iterate through each row
-
-//     for (int i = 0; i < n ; i++)
-//     {
-
-//         // assigning comparator
-
-//         int comp;
-
-//         if (i < n)
-//         {
-
-//             comp = i + 1;
-//         }
-
-//         else
-//         {
-
-//             comp = (n - i) - 3;
-//         }
-
-//         // first inner loop to print leading whitespaces
-
-//         for (int j = 0; j < comp; j++)
-//         {
-
-//             printf(" ");
-//         }
-
-//         // second inner loop to print number and inner
-
-//         // whitespaces
-
-//         for (int k = 0; k < n - comp; k++)
-//         {
-
-//             if (k == 0 || k == n - comp - 1 || i == 0 || i == n - 2)
-//             {
-
-//                 printf("%d ", k + 1);
-//             }
-
-//             else
-//             {
-
-//                 printf("  ");
-//             }
-//         }
-
-//         printf("\n");
-//     }
-
-//     return 0;
-// }
-#include <stdio.h>
-
+#include<stdio.h>
 int main()
 {
-    int rows, i, j, k;
-
-    printf("Enter Hollow Sandglass Star Pattern Rows = ");
-    scanf("%d", &rows);
-
-    printf("Printing Hollow Sandglass Star Pattern\n");
-
-    for (i = 1; i <= rows; i++)
+    int i,j,n,f;
+    printf("Enter term : ");
+    scanf("%d",&n);
+    for(i=1,f=0;i<=n;i++)
     {
-        for (j = 1; j < i; j++)
+        for(j=1;j<i;j++)
         {
             printf(" ");
         }
-        for (k = i; k <= rows; k++)
+        for(j=0;j<=n-i;j++)
         {
-            if (i == 1 || k == i || k == rows)
+            if(j==0 || j==n-i || i==1)
             {
-                printf("* ");
+                printf("%d ",f);
+                ++f;
             }
             else
             {
@@ -89,18 +24,18 @@ int main()
         }
         printf("\n");
     }
-
-    for (i = rows - 1; i >= 1; i--)
+    for(i=1,f-=2;i<n;i++)
     {
-        for (j = 1; j < i; j++)
+        for(j=1;j<n-i;j++)
         {
             printf(" ");
         }
-        for (k = i; k <= rows; k++)
+        for(j=0;j<=i;j++)
         {
-            if (i == 1 || k == i || k == rows)
+            if(j==0 || j==i || i==n-1)
             {
-                printf("* ");
+                printf("%d ",f);
+                f--;
             }
             else
             {
@@ -109,4 +44,13 @@ int main()
         }
         printf("\n");
     }
+    return 0;
 }
+// Enter term : 4
+// 0 1 2 3 
+//  4   5 
+//   6 7 
+//    8 
+//   7 6 
+//  5   4 
+// 3 2 1 0
